44c5abd98648167d6f0352cb9d8e91de8d582685
waire-p <arzhaevaalisa@gmail.com>
2025-11-01 15:22:28 +0300

    + настройки сохраняются после перезапуска и после нажатия кнопок

--- data/settings_widget.py
+++ data/settings_widget.py
@@ -3,6 +3,7 @@ from PySide6.QtWidgets import QWidget
 from ui.ui_settings import Ui_Settings
 import json
 
+
 class Settings(QWidget):
     def __init__(self):
         super().__init__()
@@ -11,24 +12,23 @@ class Settings(QWidget):
 
         self.ui.save_button.clicked.connect(self.save_settings)
         self.ui.reset_button.clicked.connect(self.reset_settings)
-        with open('data/settings.json') as file:
-            settings = json.load(file)
-            print(settings)
+        self.change_values()
 
     def save_settings(self):
         with open('data/settings.json', mode='w') as file:
-            new_settings = {}
-            new_settings['screen_lock'] = self.ui.screen_lock.isChecked()
-            new_settings['break_skip'] = self.ui.break_skip.isChecked()
-            new_settings['mode'] = self.ui.break_mode.checkedButton().objectName()
-            hours, minutes, seconds = int(self.ui.timer_hours.text()), int(self.ui.timer_minutes.text()),\
-                int(self.ui.timer_seconds.text())
-            milliseconds = hours*3600*1000+minutes*60*1000+seconds*1000
+            new_settings = {'screen_lock': self.ui.screen_lock.isChecked(),
+                            'break_skip': self.ui.break_skip.isChecked(),
+                            'mode': self.ui.break_mode.checkedButton().objectName()}
+            hours, minutes, seconds = int(self.ui.timer_hours.text()), \
+                int(self.ui.timer_minutes.text()), int(self.ui.timer_seconds.text())
+            milliseconds = hours * 3600 * 1000 + minutes * 60 * 1000 + seconds * 1000
             new_settings['timer'] = milliseconds
             minutes, seconds = int(self.ui.break_minutes.text()), int(self.ui.break_seconds.text())
             milliseconds = minutes * 60 * 1000 + seconds * 1000
             new_settings['break_time'] = milliseconds
-            json.dump(new_settings)
+            json.dump(new_settings, file)
+        self.change_values()
+        self.close()
 
     def reset_settings(self):
         with open('data/settings.json', mode='w') as file:
@@ -37,4 +37,22 @@ class Settings(QWidget):
                             'mode': 'static_image',
                             'timer': 1800000,
                             'break_time': 300000}
-            json.dump(new_settings)
+            json.dump(new_settings, file)
+        self.change_values()
+        self.close()
+
+    def change_values(self):
+        with open('data/settings.json') as file:
+            settings = json.load(file)
+            self.ui.break_skip.setChecked(settings['break_skip'])
+            self.ui.screen_lock.setChecked(settings['screen_lock'])
+            if settings['mode'] == 'static_image':
+                self.ui.static_image.setChecked(True)
+            else:
+                self.ui.video.setChecked(True)
+            self.ui.timer_hours.setValue(int(settings['timer'] / (1000 * 60 * 60)) % 24)
+            self.ui.timer_minutes.setValue(int(settings['timer'] / (1000 * 60)) % 60)
+            self.ui.timer_seconds.setValue(int(settings['timer'] / 1000) % 60)
+
+            self.ui.break_minutes.setValue(int(settings['break_time'] / (1000 * 60)) % 60)
+            self.ui.break_seconds.setValue((int(settings['break_time'] / 1000) % 60))
